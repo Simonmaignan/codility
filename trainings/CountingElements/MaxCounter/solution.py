5e6ddf9749606120d07e3b8c97e7772dbcf97163
Simon Maignan <simonmaignan88@gmail.com>
2024-01-25 18:51:39 +0100

    CountingElements MaxCounter

--- trainings/CountingElements/MaxCounter/solution.py
+++ trainings/CountingElements/MaxCounter/solution.py
@@ -3,4 +3,14 @@
 
 def solution(N, A):
     # Implement your solution here
-    pass
+    M = len(A)
+    max_counter = 0
+    counters = [0] * N
+    for i in range(M):
+        if A[i] == N + 1:
+            counters = [max_counter] * N
+        else:
+            counters[A[i] - 1] += 1
+            max_counter = max(max_counter, counters[A[i] - 1])
+
+    return counters
--- trainings/CountingElements/MaxCounter/tests/test_solution.py
+++ trainings/CountingElements/MaxCounter/tests/test_solution.py
@@ -1,5 +1,8 @@
+from datetime import datetime as dt
 from pathlib import Path
+import random
 import re
+from tkinter import N
 from typing import List, Tuple
 import unittest
 
@@ -11,7 +14,7 @@ class TestingSolution(unittest.TestCase):
         with open(Path(__file__).parent.absolute() / 'test-input.txt', 'r') as f:
             test_inputs: List[str] = f.readlines()
 
-        self._test_cases: List[Tuple[int, List[int], List[int]]] = []
+        self._validity_test_cases: List[Tuple[int, List[int], List[int]]] = []
         for test_input in test_inputs:
             test_input = test_input.strip().replace(' ', '')
             if test_input.startswith('#'):
@@ -21,14 +24,35 @@ class TestingSolution(unittest.TestCase):
                 continue
             N_regex, A_regex, result_regex =re_match.groups()
             test_input = test_input.split(']')
-            self._test_cases.append((int(N_regex),
+            self._validity_test_cases.append((int(N_regex),
                 [int(i) for i in A_regex.split(',')],
                 [int(i) for i in result_regex.split(',')]
             ))
+
+        self._efficiency_test_cases: List[Tuple[int, List[int]]] = []
+        N_min = 1
+        N_max = 100000
+        N_equivalence_classes = [N_min, random.randint(N_min, N_max - 1), N_max]
+        M_min = 1
+        M_max = 100000
+        M_equivalence_classes = [M_min, random.randint(M_min, M_max - 1), M_max]
+        for N in N_equivalence_classes:
+            for M in M_equivalence_classes:
+                A: List[int] = [random.randint(1, N + 1) for _ in range(M)]
+                self._efficiency_test_cases.append((N, A))
         return super().setUp()
     
-    def test_solution(self):
-        for test_case in self._test_cases:
-            print(test_case)
+    def test_solution_validity(self):
+        for test_case in self._validity_test_cases:
+            print("Test")
             self.assertEqual(solution(test_case[0], test_case[1]), test_case[2])
 
+    def test_solution_efficiency(self):
+        for test_case in self._efficiency_test_cases:
+            start_test = dt.now()
+            # print(f"N = {test_case[0]}")
+            # print(f"Size A = {len(test_case[1])}")
+            solution(test_case[0], test_case[1])
+            execution_time = (dt.now() - start_test).total_seconds()
+            # print(f"Execution time = {execution_time}") 
+            self.assertLess((dt.now() - start_test).total_seconds(), 0.464)
